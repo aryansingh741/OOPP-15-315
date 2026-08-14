d4d0591cd0f7298d196f2187df2158602efe1000
aryansingh741 <aryansingh69876@gmail.com>
2026-08-14 09:57:17 +0530

    initialcommit

---
+++ prg3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+class point{
+    private:
+    int x,y;
+
+    public:
+    point()
+    {
+        x=y=0;
+    }
+};
+
+
+int main(){
+
+return 0;
+}
